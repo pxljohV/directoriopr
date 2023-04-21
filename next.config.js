2ab6596d343211628aa03e76a78cd61c0615f08e
pxljohV <pixeljohann@gmail.com>
2023-04-20 23:36:16 -0500

    24

--- next.config.js
+++ next.config.js
@@ -16,7 +16,30 @@ const nextConfig = {
       },
     ];
   },
-  
+  env: {
+    USER: "u936807503_admin",
+    PW: "Q2pKMnJ9XWt",
+    DB: "u936807503_mydb",
+    HOST: "sql753.main-hosting.eu",
+    DBPORT: 3306,
+    SERV: "mysql",
+    TA: "lagente",
+    NA: "name",
+    TE: "tel",
+    TO: "town",
+    NU: "number",
+    DE: "desc",
+    KY: "keynum",
+    EM: "email",
+    API_1: "api/getlist",
+    API_RKNCK: "api/recieveknock",
+    API_PR: "/api/processemail",
+    API_CHECK: "api/checkaprove",
+    API_UP: "./api/update",
+    DEFAULT: "default_service",
+    TEMPLATE: "template_dw0t14f",
+    PK: "b04nYPQtrugI_VvpZ",
+  }
 }
 
 module.exports = nextConfig
